0cd8a2d4ac7bf238dce458c3a88144dc6f9d7b6f
Thomas Broussard <thomas.broussard@gmail.com>
2015-11-24 19:26:31 +0100

    Search method integration in the IdentityFileDaO

--- IamCore/src/fr/tbr/iamcore/dao/IdentityFileDAO.java
+++ IamCore/src/fr/tbr/iamcore/dao/IdentityFileDAO.java
@@ -10,31 +10,28 @@ import java.util.Scanner;
 
 import fr.tbr.iamcore.datamodel.Identity;
 
-
 /**
  * The main DAO for the Identity Class
+ * 
  * @author tbrou
  *
  */
 public class IdentityFileDAO {
-	
+
 	private static final String path = "/tests/iam/identities.txt";
-	
+
 	private Scanner scanner;
 	private PrintWriter writer;
-	
-	
-	
+
 	public IdentityFileDAO() throws Exception {
 		initIO();
 	}
-	
-	
+
 	/**
 	 * 
 	 * @param identity
 	 */
-	public void create(Identity identity){
+	public void create(Identity identity) {
 		writer.println("--- identity:begin ---");
 		writer.println(identity.getDisplayName());
 		writer.println(identity.getEmail());
@@ -42,16 +39,15 @@ public class IdentityFileDAO {
 		writer.println("--- identity:begin ---");
 		writer.flush();
 	}
-	
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	public List<Identity> readAll(){
+	public List<Identity> readAll() {
 		List<Identity> result = new ArrayList<Identity>();
-		while (scanner.hasNext()){
-			
+		while (scanner.hasNext()) {
+
 			scanner.nextLine();
 			String displayName = scanner.nextLine();
 			String email = scanner.nextLine();
@@ -59,39 +55,64 @@ public class IdentityFileDAO {
 			scanner.nextLine();
 			Identity id = new Identity(uid, email, displayName);
 			result.add(id);
-		}		
-		
+		}
+
 		scanner.reset();
 		return result;
 	}
-	
-	
+
+	public List<Identity> search(Identity criteria) {
+		List<Identity> resultsList = new ArrayList<Identity>();
+
+		while (scanner.hasNext()) {
+
+			scanner.nextLine();
+			String displayName = scanner.nextLine();
+			String email = scanner.nextLine();
+			String uid = scanner.nextLine();
+			scanner.nextLine();
+			Identity id = new Identity(uid, email, displayName);
+			// before to add the "id" into the list, lets check that it is
+			// corresponding to the given criteria
+
+			if (email.equals(criteria.getEmail())
+					|| displayName.equals(criteria.getDisplayName())) {
+				//it is matching, add the found identity in the resultlist
+				resultsList.add(id);
+			}
+		}
+		scanner.reset();
+		
+		
+		return resultsList;
+	}
+
 	/**
 	 * This method will check that the file exists or create it if it doesn't
+	 * 
 	 * @param pathname
 	 * @throws IOException
 	 */
 	private void initIO() throws Exception {
 		File file = new File(path);
-		if (!file.exists()){
-			//creation code after
+		if (!file.exists()) {
+			// creation code after
 			System.out.println("the file does not exists");
 			File parent = file.getParentFile();
-			if (!parent.exists()){
+			if (!parent.exists()) {
 				parent.mkdirs();
 			}
 			file.createNewFile();
 			System.out.println("file was successfully created");
-		}else{
+		} else {
 			System.out.println("the file already exists");
 		}
 		this.scanner = new Scanner(file);
-		//open the writer
+		// open the writer
 		this.writer = new PrintWriter(new FileOutputStream(file, true));
 	}
-	
-	
-	public void close(){
+
+	public void close() {
 		this.scanner.close();
 		this.writer.close();
 	}
--- IamCore/test/fr/tbr/iamcore/tests/dao/TestIdentityFileDAO.java
+++ IamCore/test/fr/tbr/iamcore/tests/dao/TestIdentityFileDAO.java
@@ -8,6 +8,41 @@ import fr.tbr.iamcore.datamodel.Identity;
 public class TestIdentityFileDAO {
 	
 	public static void main(String... args) throws Exception {
+		
+		
+		testCreateAndSearch();
+		
+		
+		
+		
+	}
+
+	
+	//TestCase
+	private static void testCreateAndSearch() throws Exception {
+		
+		IdentityFileDAO dao = new IdentityFileDAO();
+		
+		dao.create(new Identity("123", "qdc@qdc.com", "Quentin Decayeux"));
+		dao.create(new Identity("456", "sgu@qdc.com", "Sandy Guilleaumau"));
+		dao.create(new Identity("789", "cle@qdc.com", "Clément Serrano"));
+		
+		System.out.println("The input is finished");
+		System.out.println("This is what has been written : ");
+		
+		//test with partial criteria
+		System.out.println(dao.search(new Identity(null, "sgu@qdc.com", null)));
+		System.out.println(dao.search(new Identity(null, null, "Quentin Decayeux" )));
+		
+		
+		
+		dao.close();
+		
+	}
+
+
+	//TestCase
+	private static void testCreateAndReadAll() throws Exception {
 		Scanner scanner = new Scanner(System.in);
 		IdentityFileDAO dao = new IdentityFileDAO();
 		String answer = "";
@@ -24,9 +59,6 @@ public class TestIdentityFileDAO {
 		
 		dao.close();
 		scanner.close();
-		
-		
-		
 	}
 
 	
