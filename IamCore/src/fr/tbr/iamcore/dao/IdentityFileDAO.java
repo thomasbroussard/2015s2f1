12b7fc1ead719bcc7f2dd0969dcb3c6f4f6ad16f
Thomas Broussard <thomas.broussard@gmail.com>
2015-11-24 20:22:52 +0100

    *upd : added the update feature (to be optimized)

--- IamCore/src/fr/tbr/iamcore/dao/IdentityFileDAO.java
+++ IamCore/src/fr/tbr/iamcore/dao/IdentityFileDAO.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -37,7 +38,7 @@ public class IdentityFileDAO {
 		writer.println(identity.getDisplayName());
 		writer.println(identity.getEmail());
 		writer.println(identity.getUid());
-		writer.println("--- identity:begin ---");
+		writer.println("--- identity:end---");
 		writer.flush();
 	}
 
@@ -81,7 +82,7 @@ public class IdentityFileDAO {
 				resultsList.add(id);
 			}
 		}
-		//TODO find a more performant way to reset the scanner
+		//TODO find a clever way to reset the scanner
 		scanner.close();
 		scanner = new Scanner(new File(path));
 		
@@ -89,6 +90,64 @@ public class IdentityFileDAO {
 		return resultsList;
 	}
 
+	private Identity getById(String searchedUid){
+		
+		Identity resultIdentity = null;
+		while (scanner.hasNext()) {
+			scanner.nextLine();
+			String displayName = scanner.nextLine();
+			String email = scanner.nextLine();
+			String uid = scanner.nextLine();
+			scanner.nextLine();
+			Identity id = new Identity(uid, email, displayName);
+			// before to add the "id" into the list, lets check that it is
+			// corresponding to the given criteria
+			if (searchedUid.equals(uid)) {
+				//it is matching, add the found identity in the resultlist
+				resultIdentity = id;
+			}
+		}
+		return resultIdentity;
+	}
+	
+	public void update(Identity identity) throws Exception{
+		//first, search the passed identity in the file
+		Identity foundIndentity = getById(identity.getUid());
+		
+		//create an other file to save the new content
+		File file = new File(path+"-new");
+		PrintWriter newPrinter = new PrintWriter(file);
+		
+		while(scanner.hasNext()){
+			scanner.nextLine();
+			String displayName = scanner.nextLine();
+			String email = scanner.nextLine();
+			String uid = scanner.nextLine();
+			scanner.nextLine();
+			if (!identity.getUid().equals((uid))){
+				newPrinter.println("--- identity:begin ---");
+				newPrinter.println(displayName);
+				newPrinter.println(email);
+				newPrinter.println(uid);
+				newPrinter.println("--- identity:end ---");
+				newPrinter.flush();
+			}
+		}
+		newPrinter.println("--- identity:begin ---");
+		newPrinter.println(identity.getDisplayName());
+		newPrinter.println(identity.getEmail());
+		newPrinter.println(identity.getUid());
+		newPrinter.println("--- identity:end ---");
+		writer.close();
+		newPrinter.close();
+		scanner.close();
+		File oldFile = new File(path);
+		Files.delete(oldFile.toPath());
+		Files.move(file.toPath(), oldFile.toPath());
+		initIO();
+	}
+	
+	
 	/**
 	 * This method will check that the file exists or create it if it doesn't
 	 * 
